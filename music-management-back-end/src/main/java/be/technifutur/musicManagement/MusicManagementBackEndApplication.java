4f62f2f78ec81c0137161bd7b2fd3e6fff945db5
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-09 16:10:48 +0200

    Initialize back-end project

---
+++ music-management-back-end/src/main/java/be/technifutur/musicManagement/MusicManagementBackEndApplication.java
@@ -0,0 +1,13 @@
+package be.technifutur.musicManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicManagementBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicManagementBackEndApplication.class, args);
+	}
+
+}
---
+++ music-management-back-end/src/test/java/be/technifutur/musicManagement/MusicManagementBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technifutur.musicManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicManagementBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
