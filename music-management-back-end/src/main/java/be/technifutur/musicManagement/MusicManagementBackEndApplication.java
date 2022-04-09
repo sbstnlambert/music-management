f3adbb4cf24cf056a0c8e6e5f782f0f171da49f6
Lionel <Lionel.Delsupexhe@gmail.com>
2022-04-09 17:04:55 +0200

    Added ApplicationContext variable to main

--- music-management-back-end/src/main/java/be/technifutur/musicManagement/MusicManagementBackEndApplication.java
+++ music-management-back-end/src/main/java/be/technifutur/musicManagement/MusicManagementBackEndApplication.java
@@ -2,12 +2,13 @@ package be.technifutur.musicManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MusicManagementBackEndApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MusicManagementBackEndApplication.class, args);
+		ApplicationContext context = SpringApplication.run(MusicManagementBackEndApplication.class, args);
 	}
 
 }
