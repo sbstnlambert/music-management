94b9605951fa9728c32c1061f78b7e5a42d5f63a
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-12 11:39:42 +0200

    added a method to map Artist entity to DTO

---
+++ music-management-back-end/src/main/java/be/technifutur/musicManagement/business/mapper/ArtistMapper.java
@@ -0,0 +1,18 @@
+package be.technifutur.musicManagement.business.mapper;
+
+import be.technifutur.musicManagement.model.dto.ArtistDTO;
+import be.technifutur.musicManagement.model.entity.Artist;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArtistMapper {
+    public ArtistDTO entityToDTO(Artist entity) {
+            if (entity == null)
+                return null;
+
+            return ArtistDTO.builder()
+                    .id(entity.getId())
+                    .name(entity.getName())
+                    .build();
+    }
+}
