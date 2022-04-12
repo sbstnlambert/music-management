ad6592eb084c1627f069196d4c135417d03921ae
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-12 13:54:38 +0200

    bugfixed request mapping uri

--- music-management-back-end/src/main/java/be/technifutur/musicManagement/controller/ArtistController.java
+++ music-management-back-end/src/main/java/be/technifutur/musicManagement/controller/ArtistController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(name = "artist")
+@RequestMapping("/artist")
 public class ArtistController {
 
     private final ArtistService service;
