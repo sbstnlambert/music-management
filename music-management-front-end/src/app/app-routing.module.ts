3c7bb60384e03d3ffa3168e1baafb71d0dad6c46
Lionel <Lionel.Delsupexhe@gmail.com>
2022-04-12 12:01:01 +0200

    Set app routing and added bootstrap

--- music-management-front-end/src/app/app-routing.module.ts
+++ music-management-front-end/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArtistsListComponent } from './components/artist/artists-list/artists-list.component';
+import { Error404Component } from './components/error404/error404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'artist', pathMatch: 'full'},
+  { path: 'artist', component: ArtistsListComponent },
+  { path: '404', component: Error404Component },
+  { path: '**', redirectTo: '404'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- music-management-front-end/src/app/app.module.ts
+++ music-management-front-end/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ArtistsListComponent } from './components/artist/artists-list/artists-list.component';
+import { Error404Component } from './components/error404/error404.component';
+import { ArtistCardComponent } from './components/artist/artist-card/artist-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ArtistsListComponent,
+    Error404Component,
+    ArtistCardComponent
   ],
   imports: [
     BrowserModule,
