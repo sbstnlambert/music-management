971b7095f899544bde53489c611b387b1db2e15a
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-13 19:30:59 +0200

    added track detailed path

--- music-management-front-end/src/app/app-routing.module.ts
+++ music-management-front-end/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { AlbumDetailedComponent } from './components/album/album-detailed/album-
 import { ArtistDetailComponent } from './components/artist/artist-detail/artist-detail.component';
 import { ArtistsListComponent } from './components/artist/artists-list/artists-list.component';
 import { Error404Component } from './components/error404/error404.component';
+import { TrackDetailedComponent } from './components/track/track-detailed/track-detailed.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'artist', pathMatch: 'full'},
   { path: 'artist', component: ArtistsListComponent },
   { path: 'artist/:id', component: ArtistDetailComponent },
   { path: 'album/:id', component: AlbumDetailedComponent },
+  { path: 'track/:id', component: TrackDetailedComponent },
   { path: '404', component: Error404Component },
   { path: '**', redirectTo: '404'}
 ];
