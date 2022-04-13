9743b35c9db8a52f697034af33ad740d9b129b64
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-13 18:59:32 +0200

    added album path

--- music-management-front-end/src/app/app-routing.module.ts
+++ music-management-front-end/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumCardComponent } from './components/album/album-card/album-card.component';
+import { AlbumDetailedComponent } from './components/album/album-detailed/album-detailed.component';
 import { ArtistDetailComponent } from './components/artist/artist-detail/artist-detail.component';
 import { ArtistsListComponent } from './components/artist/artists-list/artists-list.component';
 import { Error404Component } from './components/error404/error404.component';
@@ -8,6 +10,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'artist', pathMatch: 'full'},
   { path: 'artist', component: ArtistsListComponent },
   { path: 'artist/:id', component: ArtistDetailComponent },
+  { path: 'album/:id', component: AlbumDetailedComponent },
   { path: '404', component: Error404Component },
   { path: '**', redirectTo: '404'}
 ];
