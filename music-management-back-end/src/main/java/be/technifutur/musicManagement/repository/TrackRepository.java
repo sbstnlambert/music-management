fddb152578b2e0b5974193f1b212abfea2079e02
Lionel <Lionel.Delsupexhe@gmail.com>
2022-04-13 16:22:35 +0200

    Created TrackRepository and specified findTracksByAlbum method

---
+++ music-management-back-end/src/main/java/be/technifutur/musicManagement/repository/TrackRepository.java
@@ -0,0 +1,13 @@
+package be.technifutur.musicManagement.repository;
+
+import be.technifutur.musicManagement.model.entity.Album;
+import be.technifutur.musicManagement.model.entity.Track;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface TrackRepository extends JpaRepository<Track, Long> {
+    @Query("SELECT t FROM Track t WHERE t.album.id = ?1")
+    List<Album> findTracksByAlbum(Long albumId);
+}
