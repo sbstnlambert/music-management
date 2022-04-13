9421d2db80e76b2ed42d3d042abe1187883cd7d7
Lionel <Lionel.Delsupexhe@gmail.com>
2022-04-13 13:17:18 +0200

    Fixed a timing bug in ArtistDetailComponent

--- music-management-front-end/src/app/components/artist/artist-detail/artist-detail.component.ts
+++ music-management-front-end/src/app/components/artist/artist-detail/artist-detail.component.ts
@@ -12,106 +12,108 @@ import { ArtistService } from 'src/app/service/artist.service';
 })
 export class ArtistDetailComponent implements OnInit {
 
-  public artist: Artist = {
-    id: 1,
-    name: 'Twenty Two Pilots',
-    imageUrl: ''
-  };
-  public albums: Array<Album> = [
-    {
-      id: 1,
-      name: 'Trench',
-      releaseDate: new Date(2018, 5),
-      recordLabel: 'Fueled By Ramen',
-      imageUrl: 'https://bit.ly/38MgZ67'
-    },
-    {
-      id: 2,
-      name: 'Trench',
-      releaseDate: new Date(2018, 5),
-      recordLabel: 'Fueled By Ramen',
-      imageUrl: 'https://bit.ly/38MgZ67'
-    },
-    {
-      id: 3,
-      name: 'Trench',
-      releaseDate: new Date(2018, 5),
-      recordLabel: 'Fueled By Ramen',
-      imageUrl: 'https://bit.ly/38MgZ67'
-    },
-    {
-      id: 4,
-      name: 'Trench',
-      releaseDate: new Date(2018, 5),
-      recordLabel: 'Fueled By Ramen',
-      imageUrl: 'https://bit.ly/38MgZ67'
-    },
-    {
-      id: 5,
-      name: 'Trench',
-      releaseDate: new Date(2018, 5),
-      recordLabel: 'Fueled By Ramen',
-      imageUrl: 'https://bit.ly/38MgZ67'
-    },
-    {
-      id: 6,
-      name: 'Trench',
-      releaseDate: new Date(2018, 5),
-      recordLabel: 'Fueled By Ramen',
-      imageUrl: 'https://bit.ly/38MgZ67'
-    },
-    {
-      id: 7,
-      name: 'Trench',
-      releaseDate: new Date(2018, 5),
-      recordLabel: 'Fueled By Ramen',
-      imageUrl: 'https://bit.ly/38MgZ67'
-    },
-    {
-      id: 8,
-      name: 'Trench',
-      releaseDate: new Date(2018, 5),
-      recordLabel: 'Fueled By Ramen',
-      imageUrl: 'https://bit.ly/38MgZ67'
-    },
-    {
-      id: 9,
-      name: 'Trench',
-      releaseDate: new Date(2018, 5),
-      recordLabel: 'Fueled By Ramen',
-      imageUrl: 'https://bit.ly/38MgZ67'
-    },
-    {
-      id: 10,
-      name: 'Trench',
-      releaseDate: new Date(2018, 5),
-      recordLabel: 'Fueled By Ramen',
-      imageUrl: 'https://bit.ly/38MgZ67'
-    },
+  public artist!: Artist;
+  //  = {
+  //   id: 1,
+  //   name: 'Twenty Two Pilots',
+  //   imageUrl: ''
+  // };
+  public albums!: Array<Album>;
+  //  = [
+  //   {
+  //     id: 1,
+  //     name: 'Trench',
+  //     releaseDate: new Date(2018, 5),
+  //     recordLabel: 'Fueled By Ramen',
+  //     imageUrl: 'https://bit.ly/38MgZ67'
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Trench',
+  //     releaseDate: new Date(2018, 5),
+  //     recordLabel: 'Fueled By Ramen',
+  //     imageUrl: 'https://bit.ly/38MgZ67'
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Trench',
+  //     releaseDate: new Date(2018, 5),
+  //     recordLabel: 'Fueled By Ramen',
+  //     imageUrl: 'https://bit.ly/38MgZ67'
+  //   },
+  //   {
+  //     id: 4,
+  //     name: 'Trench',
+  //     releaseDate: new Date(2018, 5),
+  //     recordLabel: 'Fueled By Ramen',
+  //     imageUrl: 'https://bit.ly/38MgZ67'
+  //   },
+  //   {
+  //     id: 5,
+  //     name: 'Trench',
+  //     releaseDate: new Date(2018, 5),
+  //     recordLabel: 'Fueled By Ramen',
+  //     imageUrl: 'https://bit.ly/38MgZ67'
+  //   },
+  //   {
+  //     id: 6,
+  //     name: 'Trench',
+  //     releaseDate: new Date(2018, 5),
+  //     recordLabel: 'Fueled By Ramen',
+  //     imageUrl: 'https://bit.ly/38MgZ67'
+  //   },
+  //   {
+  //     id: 7,
+  //     name: 'Trench',
+  //     releaseDate: new Date(2018, 5),
+  //     recordLabel: 'Fueled By Ramen',
+  //     imageUrl: 'https://bit.ly/38MgZ67'
+  //   },
+  //   {
+  //     id: 8,
+  //     name: 'Trench',
+  //     releaseDate: new Date(2018, 5),
+  //     recordLabel: 'Fueled By Ramen',
+  //     imageUrl: 'https://bit.ly/38MgZ67'
+  //   },
+  //   {
+  //     id: 9,
+  //     name: 'Trench',
+  //     releaseDate: new Date(2018, 5),
+  //     recordLabel: 'Fueled By Ramen',
+  //     imageUrl: 'https://bit.ly/38MgZ67'
+  //   },
+  //   {
+  //     id: 10,
+  //     name: 'Trench',
+  //     releaseDate: new Date(2018, 5),
+  //     recordLabel: 'Fueled By Ramen',
+  //     imageUrl: 'https://bit.ly/38MgZ67'
+  //   },
     
-  ];
+  // ];
 
   constructor(
     private route: ActivatedRoute, 
     private albumService: AlbumService,
     private artistService: ArtistService
   ) {
-    // let artistIdString: string | null = route.snapshot.paramMap.get('id');
-    // let artistId: number = artistIdString ? parseInt(artistIdString) : -1;
+    let artistIdString: string | null = route.snapshot.paramMap.get('id');
+    let artistId: number = artistIdString ? parseInt(artistIdString) : -1;
 
-    // if (artistId && artistId > 0) {
-    //   this.artistService.getArtistById(artistId).subscribe({
-    //     next: artist => this.artist = artist,
-    //     error: () => console.log("An error has occured while communicating with the back-end service");
-    //   });
+    if (artistId && artistId > 0) {
+      this.artistService.getArtistById(artistId).subscribe({
+        next: artist => this.artist = artist,
+        error: () => console.log("An error has occured while communicating with the back-end service")
+      });
 
-    //   this.albumService.getAlbumsByArtist(artistId).subscribe({
-    //     next: albums => this.albums = albums,
-    //     error: () => console.log("An error has occured while communicating with the back-end service");
-    //   });
-    // } else {
-    //   alert('Id invalide');
-    // }
+      this.albumService.getAlbumsByArtist(artistId).subscribe({
+        next: albums => this.albums = albums,
+        error: () => console.log("An error has occured while communicating with the back-end service")
+      });
+    } else {
+      alert('Id invalide');
+    }
   }
 
   ngOnInit(): void {
