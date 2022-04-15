b8f6e8d26df6a39b654ab85bf88d645360a1de4f
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-15 11:15:56 +0200

    added custom response entity exception handler

---
+++ music-management-back-end/src/main/java/be/technifutur/musicManagement/controller/ControllerAdvisor.java
@@ -0,0 +1,29 @@
+package be.technifutur.musicManagement.controller;
+
+import be.technifutur.musicManagement.exception.ElementNotFoundException;
+import be.technifutur.musicManagement.model.dto.ErrorDTO;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ControllerAdvisor extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ElementNotFoundException.class)
+    public ResponseEntity<ErrorDTO> handleElementNotFound(ElementNotFoundException e, HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(
+                        ErrorDTO.builder()
+                                .message(e.getMessage())
+                                .method(HttpMethod.resolve(request.getMethod()))
+                                .status(404)
+                                .uri(request.getRequestURI())
+                                .build()
+                );
+    }
+}
