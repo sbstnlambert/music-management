60f108272a2ef73686fbdde4816ec70a76036739
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-15 10:47:09 +0200

    added insert artist interface method

--- music-management-back-end/src/main/java/be/technifutur/musicManagement/business/service/specification/ArtistService.java
+++ music-management-back-end/src/main/java/be/technifutur/musicManagement/business/service/specification/ArtistService.java
@@ -1,10 +1,14 @@
 package be.technifutur.musicManagement.business.service.specification;
 
 import be.technifutur.musicManagement.model.dto.ArtistDTO;
+import be.technifutur.musicManagement.model.form.ArtistForm;
 
 import java.util.List;
 
 public interface ArtistService {
+
+    // CREATE
+    ArtistDTO insertArtist(ArtistForm form);
 
     // READ
     List<ArtistDTO> getAllArtists();
