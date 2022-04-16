a1604a007b3fa34d0fd29700d7f7374658cf8105
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-16 21:05:14 +0200

    implemented insertAlbum back-end feature

--- music-management-back-end/src/main/java/be/technifutur/musicManagement/business/mapper/AlbumMapper.java
+++ music-management-back-end/src/main/java/be/technifutur/musicManagement/business/mapper/AlbumMapper.java
@@ -2,6 +2,8 @@ package be.technifutur.musicManagement.business.mapper;
 
 import be.technifutur.musicManagement.model.dto.AlbumDTO;
 import be.technifutur.musicManagement.model.entity.Album;
+import be.technifutur.musicManagement.model.entity.Artist;
+import be.technifutur.musicManagement.model.form.AlbumForm;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +19,28 @@ public class AlbumMapper {
                 .releaseDate(entity.getReleaseDate())
                 .recordLabel(entity.getRecordLabel())
                 .imageUrl(entity.getImageUrl())
+                .build();
+    }
+
+    public Album formToEntity(AlbumForm form) {
+        if (form == null)
+            return null;
+
+        // Je reçois un artistId: Long
+        // Je veux renvoyer un artist: Artist
+        // Il me faut : Artist artist = getArtistById(artistId)
+
+
+
+        return Album.builder()
+                .name(form.getName())
+                .releaseDate(form.getReleaseDate())
+                .recordLabel(form.getRecordLabel())
+                .imageUrl(form.getImageUrl())
+                .artist(
+                        Artist.builder()
+                                .id(form.getArtistId())
+                                .build())
                 .build();
     }
 
--- music-management-back-end/src/main/java/be/technifutur/musicManagement/business/service/specification/AlbumService.java
+++ music-management-back-end/src/main/java/be/technifutur/musicManagement/business/service/specification/AlbumService.java
@@ -1,10 +1,14 @@
 package be.technifutur.musicManagement.business.service.specification;
 
 import be.technifutur.musicManagement.model.dto.AlbumDTO;
+import be.technifutur.musicManagement.model.form.AlbumForm;
 
 import java.util.List;
 
 public interface AlbumService {
+
+    // CREATE
+    AlbumDTO insertAlbum(AlbumForm form);
 
     // READ
     AlbumDTO getAlbumById(Long id);
