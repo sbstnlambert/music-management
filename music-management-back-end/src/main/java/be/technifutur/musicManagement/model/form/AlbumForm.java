5f81f7a8c8a9b286fe75d278740a11266cde16a0
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-16 18:24:39 +0200

    added album form

---
+++ music-management-back-end/src/main/java/be/technifutur/musicManagement/model/form/AlbumForm.java
@@ -0,0 +1,13 @@
+package be.technifutur.musicManagement.model.form;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class AlbumForm {
+    private String name;
+    private LocalDate releaseDate;
+    private String recordLabel;
+    private String imageUrl;
+}
