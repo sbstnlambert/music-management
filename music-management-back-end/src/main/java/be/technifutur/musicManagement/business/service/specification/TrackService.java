977bad34e22f590e1380279200eff83f414c27b5
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-17 18:23:11 +0200

    added insertTrack service method

--- music-management-back-end/src/main/java/be/technifutur/musicManagement/business/service/specification/TrackService.java
+++ music-management-back-end/src/main/java/be/technifutur/musicManagement/business/service/specification/TrackService.java
@@ -2,10 +2,14 @@ package be.technifutur.musicManagement.business.service.specification;
 
 import be.technifutur.musicManagement.model.dto.TrackDetailedDTO;
 import be.technifutur.musicManagement.model.dto.TrackSimpleDTO;
+import be.technifutur.musicManagement.model.form.TrackForm;
 
 import java.util.List;
 
 public interface TrackService {
+
+    // CREATE
+    TrackSimpleDTO insertTrack(TrackForm form);
 
     // READ
     TrackDetailedDTO getTrackById(Long id);
