ec3e8a369c8e5ad7f58429654636118af836c48b
Lionel <Lionel.Delsupexhe@gmail.com>
2022-04-19 10:20:57 +0200

    Added debounceTime(1000) to ArtistsList to avoid sending too many requests

--- music-management-front-end/src/app/components/artist/artists-list/artists-list.component.ts
+++ music-management-front-end/src/app/components/artist/artists-list/artists-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { debounceTime } from 'rxjs';
 import { Artist } from 'src/app/model/artist.model';
 import { ArtistService } from 'src/app/service/artist.service';
 import { SearchService } from 'src/app/service/search.service';
@@ -17,20 +18,13 @@ export class ArtistsListComponent implements OnInit {
     private searchService: SearchService,
     private router: Router
   ) {
-    this.searchService.searchSubject.subscribe({
-      next: search => {
-        this.artistService.getArtistsByNameWithAutocomplete(search).subscribe({
-          next: artists => this.artists = artists,
-          error: () => console.log("An error has occured during the communication with the back-end service")
-        });
-      },
-      error: () => console.log("Subscription to SearchService failed")
-    });
-
     this.artistService.refreshSubject.subscribe({
       next: () => {
         this.artistService.getAllArtists().subscribe({
-          next: artists => this.artists = artists,
+          next: artists => {
+            this.artists = artists;
+            console.log(1);
+          },
           error: () => console.log("An error has occured during the communication with the back-end service")
         });
       },
@@ -38,7 +32,22 @@ export class ArtistsListComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.searchService.searchSubject.pipe(
+      debounceTime(1000)
+    ).subscribe({
+      next: search => {
+        this.artistService.getArtistsByNameWithAutocomplete(search).subscribe({
+          next: artists => {
+            this.artists = artists;
+            console.log(2);
+          },
+          error: () => console.log("An error has occured during the communication with the back-end service")
+        });
+      },
+      error: () => console.log("Subscription to SearchService failed")
+    });
+  }
 
   public onClick(): void {
     this.searchService.resetSearch();
