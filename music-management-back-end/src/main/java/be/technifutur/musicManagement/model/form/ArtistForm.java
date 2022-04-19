11484ef407d7d25f3d266b110339288c8a3a6db5
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-19 16:08:47 +0200

    added validation annotations to artist form & entity

--- music-management-back-end/src/main/java/be/technifutur/musicManagement/model/form/ArtistForm.java
+++ music-management-back-end/src/main/java/be/technifutur/musicManagement/model/form/ArtistForm.java
@@ -2,8 +2,16 @@ package be.technifutur.musicManagement.model.form;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 public class ArtistForm {
+
+    @NotBlank(message = "Name may not be blank")
+    @Size(max = 255, message = "Name must be 255 characters at maximum")
     private String name;
+
+    @Size(max = 255, message = "Image URL must be 255 characters at maximum")
     private String imageUrl;
 }
