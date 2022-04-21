38c7dc18fa7f0ce3fb73b238b5ac26ab3d8b6d78
Lionel <Lionel.Delsupexhe@gmail.com>
2022-04-21 16:57:57 +0200

    Added like and dislike buttons to tracks

--- music-management-front-end/src/app/components/track/track-banner/track-banner.component.ts
+++ music-management-front-end/src/app/components/track/track-banner/track-banner.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
+import { faHeart as fasHeart } from '@fortawesome/free-solid-svg-icons';
 import { TrackSimple } from 'src/app/model/track.model';
 import { AuthService } from 'src/app/service/auth.service';
 import { SearchService } from 'src/app/service/search.service';
 import { TrackService } from 'src/app/service/track.service';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-track-banner',
@@ -15,9 +18,13 @@ export class TrackBannerComponent implements OnInit {
 
   // Font Awesome icons
   faTrashCan = faTrashCan
-  iconColor: string = '#ff7c7c';
+
+  deleteIconColor: string = '#ff7c7c';
+  likeIconColor: string = '#ff7c7c';
   
   isHovered: boolean = false;
+  isLikeHovered: boolean = false;
+  isLiked: boolean = true;
 
   @Input()
   track!: TrackSimple;
@@ -26,8 +33,12 @@ export class TrackBannerComponent implements OnInit {
     private router: Router,
     private searchService: SearchService,
     private trackService: TrackService,
-    public authService: AuthService
-  ) { }
+    public authService: AuthService,
+    library: FaIconLibrary
+  ) {
+    // Add multiple icons to the library
+    library.addIcons(farHeart, fasHeart);
+  }
 
   ngOnInit(): void {
   }
@@ -38,7 +49,7 @@ export class TrackBannerComponent implements OnInit {
   }
 
   public onHoverDeleteButton(): void {
-    this.iconColor = (this.iconColor === '#ff7c7c' ? 'red' : '#ff7c7c');
+    this.deleteIconColor = (this.deleteIconColor === '#ff7c7c' ? 'red' : '#ff7c7c');
   }
 
   public onHover(): void {
@@ -50,6 +61,14 @@ export class TrackBannerComponent implements OnInit {
       next: track => console.log(`Track of id = ${track.id} has been successfully deleted`),
       error: () => console.log('An error has occured during the communication with the back-end service')
     });
+  }
+
+  public onHoverLikeButton(): void {
+    this.likeIconColor = (this.likeIconColor === '#ff7c7c' ? 'red' : '#ff7c7c');
+  }
+
+  public onLike(): void {
+    this.isLiked = !this.isLiked;
   }
 
 }
