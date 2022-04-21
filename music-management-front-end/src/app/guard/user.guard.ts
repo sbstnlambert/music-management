2e09d64575e1f85654e78c4c5e97aa8e077cc6ca
sbstnlambert <sbstn.lambert@gmail.com>
2022-04-21 15:18:26 +0200

    added user guard

---
+++ music-management-front-end/src/app/guard/user.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../service/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      let isUser: boolean = this.authService.isAuthenticated 
+      && this.authService.userRoles.includes('ROLE_USER');
+
+      if (!isUser) {
+        this.router.navigate(['signin']);
+      }
+    return isUser;
+  }
+  
+}
