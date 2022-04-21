54a98f99f559315725a2147fefe4b7cb65ad61b0
Lionel <Lionel.Delsupexhe@gmail.com>
2022-04-21 15:03:56 +0200

    Implemented getTracksByPlaylists(Playlist)

--- music-management-back-end/src/main/java/be/technifutur/musicManagement/repository/TrackRepository.java
+++ music-management-back-end/src/main/java/be/technifutur/musicManagement/repository/TrackRepository.java
@@ -1,9 +1,11 @@
 package be.technifutur.musicManagement.repository;
 
+import be.technifutur.musicManagement.model.entity.Playlist;
 import be.technifutur.musicManagement.model.entity.Track;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Arrays;
 import java.util.List;
 
 public interface TrackRepository extends JpaRepository<Track, Long> {
@@ -13,6 +15,8 @@ public interface TrackRepository extends JpaRepository<Track, Long> {
 
     @Query("SELECT t FROM Track t WHERE t.artist.id = ?1")
     List<Track> findTracksByArtist(Long artistId);
+
+    List<Track> findByPlaylists(Playlist playlist);
 
     @Query("SELECT t FROM Track t WHERE LOWER(t.name) LIKE LOWER(CONCAT('%',?1,'%'))")
     List<Track> findTracksByNameWithAutocomplete(String nameFragment);
